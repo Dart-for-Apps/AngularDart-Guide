619e6e67b758cbcafd0c347ed0d018a2237561b1
wurikiji <wurikiji@gmail.com>
2019-01-28 23:30:42 +0900

    Component-provided service injection
    - It has limited life time until the component instance is destroyed.

--- lib/src/heroes/heroes_component.dart
+++ lib/src/heroes/heroes_component.dart
@@ -1,6 +1,7 @@
 import 'package:angular/angular.dart';
 
 import 'hero_list_component.dart';
+import 'hero_service.dart';
 
 @Component(
   selector: 'my-heroes',
@@ -8,6 +9,7 @@ import 'hero_list_component.dart';
     <h2>Heroes</h2>
     <hero-list></hero-list>
     ''',
+    providers: [ClassProvider(HeroService)],
     directives: [HeroListComponent],
 )
 class HeroesComponent{}
