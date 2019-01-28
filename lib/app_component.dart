b4f7cd29a8181212b78455c19b78f665d7802386
wurikiji <wurikiji@gmail.com>
2019-01-28 23:27:44 +0900

    Boilerplate for dependency injection practice
    next: https://webdev.dartlang.org/angular/guide/dependency-injection#register-a-service-provider

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,6 +1,7 @@
 import 'package:angular/angular.dart';
 
-import 'src/hero_form_component.dart';
+import 'src/heroes/heroes_component.dart';
+//import 'src/hero_form_component.dart';
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
 
@@ -8,7 +9,8 @@ import 'src/hero_form_component.dart';
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [HeroFormComponent],
+//  directives: [HeroFormComponent],
+  directives: [HeroesComponent],
 )
 class AppComponent {
 }
---
+++ lib/src/heroes/heroes_component.dart
@@ -0,0 +1,13 @@
+import 'package:angular/angular.dart';
+
+import 'hero_list_component.dart';
+
+@Component(
+  selector: 'my-heroes',
+  template: '''
+    <h2>Heroes</h2>
+    <hero-list></hero-list>
+    ''',
+    directives: [HeroListComponent],
+)
+class HeroesComponent{}
