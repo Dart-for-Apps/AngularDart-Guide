64cb4107c582ac7ba8295ec3ceb40ac6ad2d4da3
wurikiji <wurikiji@gmail.com>
2019-02-07 01:30:27 +0900

    Merge code

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -3,6 +3,7 @@ import 'package:angular/angular.dart';
 import 'src/logger_service.dart';
 import 'src/heroes/heroes_component.dart';
 import 'src/heroes/user_service.dart';
+import 'src/hero_form_component.dart';
 
 @Component(
   selector: 'my-app',
@@ -12,7 +13,7 @@ import 'src/heroes/user_service.dart';
     ClassProvider(UserService),
     ClassProvider(Logger, useClass: EvenBetterLogger),
   ],
-  directives: [HeroesComponent],
+  directives: [HeroesComponent, HeroFormComponent,],
 )
 class AppComponent {
 }
